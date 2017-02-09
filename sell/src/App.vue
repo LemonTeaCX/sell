770a026689cb2330b44171d988daddb75681f423
lemon <1406428663@qq.com>
2017-02-09 23:17:48 +0800

    添加v-header组件，App.vue引用v-header组件

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,29 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-    <!-- <Hello></Hello> -->
+    <v-header></v-header>
   </div>
 </template>
 
 
 <script>
-import Hello from './components/Hello'
+import header from './components/header/header'
 export default {
-  name: 'app',
   components: {
-    Hello
+    'v-header': header
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ sell/src/components/header/header.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>this is a header</h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
