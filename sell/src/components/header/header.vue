965d72d4b2ea67dd5cbb68d636ee273318317795
lemon <1406428663@qq.com>
2017-02-12 16:52:09 +0800

    书写header组件

--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -1,10 +1,168 @@
 <template>
-  <h1>this is a header</h1>
+  <div class="header">
+    <div class="content-wrapper">
+      <div class="avatar">
+        <img :src="seller.avatar" width="64" height="64" />
+      </div>
+      <div class="content">
+        <div class="title">
+          <span class="brand"></span>
+          <span class="name">{{seller.name}}</span>
+        </div>
+        <div class="description">
+          {{seller.description + '/' + seller.deliveryTime + '分钟送达'}}
+        </div>
+        <div v-if="seller.supports" class="supports">
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
+          <span class="text">{{seller.supports[0].description}}</span>
+        </div>
+      </div>
+      <div class="supports-count" v-if="seller.supports">{{seller.supports.length+'个'}}</div>
+    </div>
+    <div class="bulletin-wrapper">
+      <div class="bulletin-title"></div>
+      <div class="bulletin-text" v-if="seller.bulletin">{{seller.bulletin}}</div>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%"/>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+  },
+  props: {
+    seller: {
+      type: Object
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '../../common/sass/index.scss';
+.header {
+  position: relative;
+  color: #fff;
+  background: rgba(7,17,27,0.5);
+  .content-wrapper {
+    padding: 24px 12px 18px 24px;
+    overflow: hidden;
+    position: relative;
+    .avatar {
+      float: left;
+      border-radius: 2px;
+      overflow: hidden;
+      img {
+        display: block;
+      }
+    }
+    .content {
+      float: left;
+      margin-left: 16px;
+      line-height: 12px;
+      .title {
+        overflow: hidden;
+        .brand {
+          float: left;
+          width: 30px;
+          height: 18px;
+          @include bg-img (brand);
+          background-size: 30px 18px;
+          background-repeat: no-repeat;
+        }
+        .name {
+          float: left;
+          margin-left: 6px;
+          font-size: 16px;
+          font-weight: bold;
+          line-height: 18px;
+        }
+      }
+      .description {
+        margin: 8px 0px 10px;
+        font-size: 12px;
+      }
+      .supports {
+        overflow: hidden;
+        .icon {
+          float: left;
+          width: 12px;
+          height: 12px;
+          &.decrease {
+              @include bg-img ('decrease_1')
+          }
+          &.discount {
+              @include bg-img ('discount_1')
+          }
+          &.guarantee {
+              @include bg-img ('guarantee_1')
+          }
+          &.invoice {
+              @include bg-img ('invoice_1')
+          }
+          &.special {
+              @include bg-img ('special_1')
+          }
+          background-size: 12px 12px;
+          background-repeat: no-repeat;
+        }
+        .text {
+          float: left;
+          margin-left: 8px;
+          font-size: 10px;
+        }
+      }
+    }
+    .supports-count {
+        width: 40px;
+        height: 24px;
+        background-color: rgba(0,0,0,0.2);
+        border-radius: 12px;
+        text-align: center;
+        font-size: 10px;
+        line-height: 24px;
+        position: absolute;
+        right: 18px;
+        bottom: 18px;
+      }
+  }
+  .bulletin-wrapper {
+    height: 28px;
+    background-color: rgba(7,17,27,0.2);
+    padding: 0px 12px;
+    line-height: 28px;
+    font-size: 10px;
+    overflow: hidden;
+    .bulletin-title {
+      float: left;
+      margin-top: 8px;
+      width: 22px;
+      height: 12px;
+      @include bg-img (bulletin);
+      background-size: 22px 12px;
+      background-repeat: no-repeat;
+    }
+    .bulletin-text {
+      float: left;
+      width: 85%;
+      margin-left: 8px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
+  .background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    filter: blur(10px);
+    z-index: -1;
+  }
+}
 </style>
