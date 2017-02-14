58856d212faa5ad40c77b034eaac959a1d86e846
lemon <1406428663@qq.com>
2017-02-14 22:56:25 +0800

    添加linetitle组件

---
+++ sell/src/components/linetitle/linetitle.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="title">
+    <div class="line"></div>
+    <div class="text">{{tit}}</div>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tit: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+  width: 80%;
+  display: flex;
+  margin: 28px auto 24px;
+  .line {
+    flex: 1;
+    height: 1px;
+    margin-top: 5px;
+    background: rgba(255,255,255,0.2);
+  }
+  .text {
+    flex: 1;
+    padding: 0 12px;
+    text-align: center;
+  }
+}
+</style>
