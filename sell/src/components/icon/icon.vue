9ecd4d2ece5fd0a5ecd697be951f961055af1449
lemon <1406428663@qq.com>
2017-02-20 01:39:16 +0800

    添加icon组件

---
+++ sell/src/components/icon/icon.vue
@@ -0,0 +1,76 @@
+<template>
+  <span class="icon" :class="classMap[type]"></span>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: Number
+    },
+    num: {
+      type: Number
+    }
+  },
+  created() {
+    this.classMap = ['decrease' + this.num, 'discount' + this.num, 'special' + this.num, 'invoice' + this.num, 'guarantee' + this.num]
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../common/sass/mixin.scss";
+
+.icon {
+  float: left;
+  width: 12px;
+  height: 12px;
+  &.decrease1 {
+      @include bg-img ('decrease_1')
+  }
+  &.decrease2 {
+      @include bg-img ('decrease_2')
+  }
+  &.decrease3 {
+      @include bg-img ('decrease_3')
+  }
+  &.discount1 {
+      @include bg-img ('discount_1')
+  }
+  &.discount2 {
+      @include bg-img ('discount_2')
+  }
+  &.discount3 {
+      @include bg-img ('discount_3')
+  }
+  &.guarantee1 {
+      @include bg-img ('guarantee_1')
+  }
+  &.guarantee2 {
+      @include bg-img ('guarantee_2')
+  }
+  &.guarantee3 {
+      @include bg-img ('guarantee_3')
+  }
+  &.invoice1 {
+      @include bg-img ('invoice_1')
+  }
+  &.invoice2 {
+      @include bg-img ('invoice_2')
+  }
+  &.invoice3 {
+      @include bg-img ('invoice_3')
+  }
+  &.special1 {
+      @include bg-img ('special_1')
+  }
+  &.special2 {
+      @include bg-img ('special_2')
+  }
+  &.special3 {
+      @include bg-img ('special_3')
+  }
+  background-size: 12px 12px;
+  background-repeat: no-repeat;
+}
+</style>
